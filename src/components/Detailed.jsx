9dfe6d77d075ebcc0ae3e489c4e6f40c613cdc51
Awarth <adarshtiwari8808@gmail.com>
2024-06-22 03:26:56 +0530

    final push

--- src/components/Detailed.jsx
+++ src/components/Detailed.jsx
@@ -10,7 +10,7 @@ function Detailed() {
 
   useEffect(() => {
     if (!article) {
-      navigate('/');
+      navigate("/");
     }
   }, [article, navigate]);
 
@@ -21,7 +21,9 @@ function Detailed() {
   return (
     <div className="article-detail flex flex-col py-2 my-4 justify-center items-center max-full p-4 bg-white">
       <div className="shadow-md max-w-lg rounded-lg p-4">
-        <h1 className="text-3xl max-450:text-2xl font-bold mb-3">{article.title}</h1>
+        <h1 className="text-3xl max-450:text-2xl font-bold mb-3">
+          {article.title}
+        </h1>
         <p className="text-gray-600 pt-2 w-full border-t">
           <span className="font-semibold">Published at:</span>{" "}
           {new Date(article.published).toLocaleDateString()}
@@ -34,11 +36,16 @@ function Detailed() {
           alt={article.title}
           className="w-full object-cover pb-4 border-b"
         />
-        <div className="text-gray-700 leading-relaxed">
-          {article.content}
+        <div className="text-gray-700 leading-relaxed mb-4">
+          {article.description}
         </div>
         <p className="text-gray-600">Read here:</p>
-        <a href={article.url} className="text-blue-500" target="_blank" rel="noopener noreferrer">
+        <a
+          href={article.url}
+          className="text-blue-500"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Click me
         </a>
       </div>
