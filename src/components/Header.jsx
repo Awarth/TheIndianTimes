34edbf9896f3f9ecb154554398934be8c9fc1840
Awarth <adarshtiwari8808@gmail.com>
2024-07-11 20:03:38 +0530

    added bg color to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <>
-      <header className="flex flex-col justify-center items-center text-[#132643] border-b-2 border-black">
+      <header className="flex flex-col justify-center items-center text-[#132643] border-b-2 border-black bg-white">
         <Link to="/">
           <h1 className="logo w-full flex justify-center items-center text-6xl max-450:text-5xl max-350:text-4xl bg-white my-2 ">
             The Bharat Times
